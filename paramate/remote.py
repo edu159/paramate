97754056e43ca3235257b47a096ee0e504098c0d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 09:10:57 +0000

    Adding tarfile member sanitization to extractall()

--- paramate/remote.py
+++ paramate/remote.py
@@ -379,7 +379,26 @@ class StudyManager():
 
     def _decompress(self, src_path, dest_path):
         with tarfile.open(src_path, "r:gz") as tar:
-            tar.extractall(dest_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, dest_path)
         
     def _cases_regexp(self):
         regexp = ""
